460b8769dca146c1312c8eb2dd5f3da231242d89
rakesh914327 <115528168+rakesh914327@users.noreply.github.com>
2024-09-03 08:47:38 +0530

    Update AadharDetails.java

--- CJD-M16/BASICS/AadharDetails.java
+++ CJD-M16/BASICS/AadharDetails.java
@@ -1,16 +1,1 @@
-class AadharDetails{
 
-	public static void main(String[] args){
-
-		System.out.println("                Aadhar Details                       ");
-		System.out.println("-----------------------------------------------------");
-		String name = "Rakesh Kothapally";
-		String dob = "27-Mar-1998";
-		String address = "8-7-98/84/BH/5, Bharathi Nagar Colony, Hasthinapuram, Pincode:500079";
-		String aadharNumber = "3455 3443 5518";
-		System.out.println("Name:" + name);
-		System.out.println("DOB:" + dob);
-		System.out.println("Address:" + address);
-		System.out.println("Aadhar No:" + aadharNumber);
-}
-}
