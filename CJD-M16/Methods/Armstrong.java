9a0cb5194cd7cd8d66ce1fa7edd27899c073236e
rakesh914327 <115528168+rakesh914327@users.noreply.github.com>
2024-10-01 20:48:58 +0530

    Update Armstrong.java

--- CJD-M16/Methods/Armstrong.java
+++ CJD-M16/Methods/Armstrong.java
@@ -4,7 +4,8 @@ public class Armstrong {
     while(n1>0){
       res=n1%10;
       int product = 1;
-      for(int i=1;i<=3;i++){
+      int count = Count(n);
+      for(int i=1;i<=count;i++){
         product=product*res;
       }
       sum=sum+product;
@@ -15,5 +16,15 @@ public class Armstrong {
   public static void main(String[] args){
     int n=153;
     System.out.println(ArmStrong(n)?"ArmStrong Number":"Not ArmStrog Number");
+  }
+  public static int Count(int n){
+    int res,count=0;
+    while (n>0){
+      res=n%10;
+      n=n/10;
+      count++;
+      
+    }
+    return count;
   }
 }
